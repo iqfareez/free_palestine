131eb782a7fd3ea265332370c1159396f480eb6f
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2025-08-04 22:08:11 +0800

    ✨ add safe area to rotate prompt
    make flag show fullscreen on android

--- lib/components/rotate_prompt.dart
+++ lib/components/rotate_prompt.dart
@@ -7,22 +7,24 @@ class RotatePrompt extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 13),
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          if (constraints.maxHeight > constraints.maxWidth) {
-            return Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                  color: Colors.white.withAlpha(200),
-                  borderRadius: BorderRadius.circular(18)),
-              child: const Text('Rotate your phone'),
-            );
-          } else {
-            return const SizedBox.shrink();
-          }
-        },
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 13),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            if (constraints.maxHeight > constraints.maxWidth) {
+              return Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    color: Colors.white.withAlpha(200),
+                    borderRadius: BorderRadius.circular(18)),
+                child: const Text('Rotate your phone'),
+              );
+            } else {
+              return const SizedBox.shrink();
+            }
+          },
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'palestine_flag.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Hide status bar
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
